ad2bb5608159922d015ab41cd466d82ac42d56b3
biplob2358 <biplob2358@gmail.com>
2025-06-20 03:49:33 +0600

    Avoid waiting

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,25 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { Server } from 'http';
-import app from './app';
-import mongoose from 'mongoose';
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
 
 let server: Server;
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/library';
+  process.env.MONGODB_URI || "mongodb://localhost:27017/library";
 
 async function main() {
   try {
-    server = app.listen(PORT, async () => {
-      await mongoose.connect(MONGODB_URI);
-      console.log('✅ Connected to Database');
+    await mongoose.connect(MONGODB_URI);
+    console.log("✅ Connected to Database");
+
+    server = app.listen(PORT, () => {
       console.log(`🚀 Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.log('🚀 ~ main ~ error:', error);
+    console.log("🚀 ~ main ~ error:", error);
   }
 }
 main();
